8de753e74f20e2173e419a363f7e69941ca7201c
SmithyySHU <c0033267@hallam.shu.ac.uk>
2023-03-23 10:49:35 +0000

    Route JS Completed functionality!

--- js/routes.js
+++ js/routes.js
@@ -3,48 +3,49 @@ var Routes = [];
 async function fetchroutes() {
 
     await fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route")
-    .then(async function (response) {
-        if (response.ok) {
-            return await response.json();
-        }
-        throw new Error(response.status + ' ' + response.statusText);
-    })
-    .then(function (routedata) {
-       
-        let htmlR =  '<table border="1"><tr><th>Route Id</th><th>Route Starting Point</th><th>Route End Point</th></tr>'
-        
-       routedata.forEach(route => {
-        console.log(route.Id)
+        .then(async function (response) {
+            if (response.ok) {
+                return await response.json();
+            }
+            throw new Error(response.status + ' ' + response.statusText);
+        })
+        .then(function (routedata) {
+
+            let htmlR = '<table border="1"><tr><th>Route Id</th><th>Route Starting Point</th><th>Route End Point</th></tr>'
+
+            routedata.forEach(route => {
+                console.log(route.Id)
 
 
-        htmlR+="<tr>"
-        htmlR+= "<td>" + route.Id + "</td>"
-        htmlR+= "<td>" + route.RouteStartPoint + "</td>"
-        htmlR+= "<td>" + route.RouteEndPoint + "</td>"
-        htmlR+= "<td><button onclick='deleteroute(" + route.Id +")'> Cancel Trip </button></td>";
-        htmlR+="<tr>"
-
-        
-        
-       })
-       htmlR += "</table>"
-       document.getElementById("RouteOut").innerHTML = htmlR
+                htmlR += "<tr>"
+                htmlR += "<td>" + route.Id + "</td>"
+                htmlR += "<td>" + route.RouteStartPoint + "</td>"
+                htmlR += "<td>" + route.RouteEndPoint + "</td>"
+                htmlR += "<td><button onclick='DisplayEditRoute(" + route.Id + ")'> Edit Route </button></td>";
+                htmlR += "<td><button onclick='deleteroute(" + route.Id + ")'> Cancel Route </button></td>";
+                htmlR += "<tr>"
 
 
-    })
 
-    .catch(function (error){
-        displayError(error);
+            })
+            htmlR += "</table>"
+            document.getElementById("RouteOut").innerHTML = htmlR
 
-    });
-    
+
+        })
+
+        .catch(function (error) {
+            displayError(error);
+
+        });
+
 }
 
 function displayError(error) {
     document.getElementById("showError").innerHTML = error;
 }
 
-function loadDoc(){
+function loadDoc() {
     fetchroutes()
 
 }
@@ -52,85 +53,158 @@ function loadDoc(){
 
 loadDoc()
 
-function addRoute(){
+function addRoute() {
     var newroute = JSON.stringify({
         RouteStartPoint: document.getElementById("newStartPoint").value,
         RouteEndPoint: document.getElementById("newEndPoint").value
-      });
+    });
 
 
-      fetch ("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route", {
-            method: "POST",
-            headers: {"Content-type": "application/json"},
-            body: newroute
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: newroute
 
-      })
+    })
 
-      .then(function (response){
+        .then(function (response) {
             if (response.ok) {
                 fetchroutes();
-                    var showFrom = document.getElementById("addRoute");
-                    var hideForm = document.getElementById("hideFrom");
-                  
-                    showFrom.style.display = "none";
-                    hideForm.style.display = "block";
-                  
-                
+                var showFrom = document.getElementById("addRoute");
+                var hideForm = document.getElementById("hideFrom");
+
+                showFrom.style.display = "none";
+                hideForm.style.display = "block";
+
+
             }
             else {
-            
-            throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
             }
 
-      })
+        })
 
-      .catch(function (error) {
-        displayError(error);
-      });
+        .catch(function (error) {
+            displayError(error);
+        });
 
 }
 
 function showAddForm() {
     var showFrom = document.getElementById("addRoute");
     var hideForm = document.getElementById("hideFrom");
-  
+
     showFrom.style.display = "block";
     hideForm.style.display = "none";
-  }
+}
 
 
 
-  function deleteroute(routeId){
-    fetch ("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route/" + routeId, {
+function deleteroute(routeId) {
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route/" + routeId, {
         method: "DELETE"
 
     })
         .then(function (response) {
-            if (response.ok){
+            if (response.ok) {
                 fetchroutes();
             }
-        else {
-        
-        throw new Error('Oops. ' + response.status + ' ' + response.statusText);
-        }
+            else {
 
-  })
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+            }
+
+        })
         .catch(function (error) {
             displayError(error);
+        });
+
+}
+
+
+async function DisplayEditRoute(routeId) {
+    await fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route/" + routeId)
+        .then(async function (response) {
+            if (response.ok) {
+                return await response.json();
+            }
+            throw new Error(response.status + ' ' + response.statusText);
+        })
+        .then(function (routedata) {
+            document.getElementById("editrouteID").value = routedata.Id
+
+
+            document.getElementById("editRouteStartPoint").value = routedata.RouteStartPoint;
+            document.getElementById("editRouteEndPoint").value = routedata.RouteEndPoint;
+
+            var showFrom = document.getElementById("DisplayEditRoute");
+                var hideForm = document.getElementById("hideFrom");
+
+                showFrom.style.display = "block";
+                hideForm.style.display = "none";
+
+
+
+
+        })
+
+        .catch(function (error) {
+            displayError(error);
+        });
+
+
+
+}
+
+async function seditRoute() {
+
+    var seditRoute = JSON.stringify({
+        Id: document.getElementById("editrouteID").value,
+        RouteStartPoint: document.getElementById("editRouteStartPoint").value,
+        RouteEndPoint: document.getElementById("editRouteEndPoint").value
+
     });
 
-  }
+
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/route", {
+
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: seditRoute
+
+    })
+        .then(function (response) {
+            if (response.ok) {
+                fetchroutes();
+                var showFrom = document.getElementById("DisplayEditRoute");
+                var hideForm = document.getElementById("hideFrom");
+
+                showFrom.style.display = "none";
+                hideForm.style.display = "block";
 
 
-  function cancelAdd(){
-    var addBooking = document.getElementById("addBooking");
-    if (addBooking) {
-        addBooking.style.display = "none";
+            }
+            else {
+
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+            }
+
+        })
+
+
+}
+
+
+function cancelAdd() {
+    var addRoute = document.getElementById("addRoute");
+    if (addRoute) {
+        addRoute.style.display = "none";
     }
 
-    var showBookings = document.getElementById("showBookings");
-    if (showBookings) {
-        showBookings.style.display = "block";
+    var hideForm = document.getElementById("hideForm");
+    if (hideForm) {
+        hideForm.style.display = "block";
     }
 }
 
