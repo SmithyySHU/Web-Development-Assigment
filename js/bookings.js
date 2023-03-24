dbd942cfb67744d2ab1da40f7b7bc072809e3eb9
SmithyySHU <c0033267@hallam.shu.ac.uk>
2023-03-24 00:55:52 +0000

    Booking completed. Next is validation

--- js/bookings.js
+++ js/bookings.js
@@ -1,140 +1,230 @@
 var Bookings = [];
 
-function init() {
-  Bookings = [
-    {
-      id: 1,
-      passName: "Sean Paul",
-      destination: "Sheffield",
-      noofPass: "3",
-      TimeRequest: new Date("2023-03-05T12:00:00Z"),
-    },
-    {
-      id: 2,
-      passName: "Jacob Sartorius",
-      destination: "London",
-      noofPass: "1",
-      TimeRequest: new Date("2023-04-05T12:20:00Z"),
-    },
-    {
-      id: 4,
-      passName: "Bonnie Smith",
-      destination: "Derby",
-      noofPass: "2",
-      TimeRequest: new Date("2023-03-05T12:23:30Z"),
-    },
-    {
-      id: 5,
-      passName: "Jackie Stallard",
-      destination: "Sheffield",
-      noofPass: "2",
-      TimeRequest: new Date("2023-03-05T12:00:00Z"),
-    },
-  ];
-}
+async function fetchBookings() {
+    await fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking")
+        .then(async function (response) {
+            if (response.ok) {
+                return await response.json();
+            }
+            throw new Error(response.status + ' ' + response.statusText);
+        })
+        .then(function (bookingdata) {
 
-function CurrentTrips() {
-  var table = createTable(Bookings);
-  document.getElementById("CurrentTrips").innerHTML = table;
+            let htmlR = '<table border="1"><tr><th>Booking ID</th><th>Pickup Location</th><th>Drop Off Location</th><th>Current Passengers</th><th>Passenger Name</th><th>Vehicle Id</th></tr>'
+
+            bookingdata.forEach(Booking => {
+                console.log(Booking.Id)
+
+
+                htmlR += "<tr>"
+                htmlR += "<td>" + Booking.Id + "</td>"
+                htmlR += "<td>" + Booking.PickupLocation + "</td>"
+                htmlR += "<td>" + Booking.DropOffLocation + "</td>"
+                htmlR += "<td>" + Booking.CurrentPassenger + "</td>"
+                htmlR += "<td>" + Booking.PassengerName + "</td>"
+                htmlR += "<td>" + Booking.VehicleId + "</td>"
+
+                htmlR += "<td><button onclick='DisplayEditBooking(" + Booking.Id + ")'> Edit Booking </button></td>";
+                htmlR += "<td><button onclick='deletebooking(" + Booking.Id + ")'> Remove Booking  </button></td>";
+                htmlR += "<tr>"
+
+
+
+            })
+            htmlR += "</table>"
+            document.getElementById("BookingOut").innerHTML = htmlR
+
+
+        })
+
+        .catch(function (error) {
+            displayError(error);
+
+        });
+
+}
+function displayError(error) {
+    document.getElementById("showError").innerHTML = error;
 }
 
 function loadDoc() {
-  init();
-  CurrentTrips();
+    fetchBookings()
+
 }
 
-function createTable(data) {
-  var table =
-    "<table border='1'><tr><th>Passenger Name</th><th>Destination</th><th>Number Of Passengers</th><th>Scheduled For</th>";
-  for (let oi of data) {
-    table += "<tr>";
-    table += "<td>" + oi.passName + "</td>";
-    table += "<td>" + oi.destination + "</td>";
-    table += "<td>" + oi.noofPass + "</td>";
-    table += "<td>" + oi.TimeRequest.toLocaleString() + "</td>";
-    table +=
-      "<td><button onclick='remove(" +
-      JSON.stringify(oi) +
-      ")'> Cancel Trip </button></td>";
-    table += "</tr>";
-  }
-  table += "</table>";
-
-  return table;
-}
+loadDoc()
 
 function addBooking() {
-  var passName = document.getElementById("PassengerNameImp").value;
-  var destination = document.getElementById("DestinationImp").value;
-  var noofPass = document.getElementById("AmountPassengerImp").value;
-  var TimeRequest = new Date(document.getElementById("ScheduledBookingImp").value);
+    var newbooking = JSON.stringify({
+        PickupLocation: document.getElementById("newPickupLocation").value,
+        DropOffLocation: document.getElementById("newDropOffLocation").value,
+        CurrentPassenger: document.getElementById("newCurrentPassenger").value,
+        PassengerName: document.getElementById("newPassengerName").value,
+        VehicleId: document.getElementById("newVehicleId").value,
 
-  var booking = {
-    id: Bookings.length + 1,
-    passName: passName,
-    destination: destination,
-    noofPass: noofPass,
-    TimeRequest: TimeRequest,
-  };
+    });
 
-  Bookings.push(booking);
 
-  CurrentTrips();
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: newbooking
 
-  document.getElementById("PassengerNameImp").value = "";
-  document.getElementById("DestinationImp").value = "";
-  document.getElementById("AmountPassengerImp").value = "";
-  document.getElementById("ScheduledBookingImp").value = "";
+    })
 
-  document.getElementById("showBookings").style.display = "block";
-  document.getElementById("addBooking").style.display = "none";
+        .then(function (response) {
+            if (response.ok) {
+                fetchBookings();
+                var showFrom = document.getElementById("addBooking");
+                var hideForm = document.getElementById("hideFrom");
 
-  console.log(Bookings);
+                showFrom.style.display = "none";
+                hideForm.style.display = "block";
+
+
+            }
+            else {
+
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+            }
+
+        })
+
+        .catch(function (error) {
+            displayError(error);
+        });
+
 }
 
 function showAddForm() {
-  var addBooking = document.getElementById("addBooking");
-  var showBookings = document.getElementById("showBookings");
+    var showFrom = document.getElementById("addBooking");
+    var hideForm = document.getElementById("hideFrom");
 
-  addBooking.style.display = "block";
-  showBookings.style.display = "none";
+    showFrom.style.display = "block";
+    hideForm.style.display = "none";
+}
+
+function deletebooking(bookingId) {
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking/" + bookingId, {
+        method: "DELETE"
+
+    })
+        .then(function (response) {
+            if (response.ok) {
+                fetchBookings();
+            }
+            else {
+
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+            }
+
+        })
+        .catch(function (error) {
+            displayError(error);
+        });
+
+}
+
+async function DisplayEditBooking(bookingId) {
+    await fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking/" + bookingId)
+        .then(async function (response) {
+            if (response.ok) {
+                return await response.json();
+            }
+            throw new Error(response.status + ' ' + response.statusText);
+        })
+        .then(function (bookingdata) {
+            document.getElementById("editbookingID").value = bookingdata.Id
+
+            document.getElementById("editPickupLocation").value = bookingdata.PickupLocation;
+            document.getElementById("editDropOffLocation").value = bookingdata.DropOffLocation;
+            document.getElementById("editCurrentPassenger").value = bookingdata.CurrentPassenger;
+            document.getElementById("editPassengerName").value = bookingdata.PassengerName;
+            document.getElementById("editVehicleId").value = bookingdata.VehicleId;
+
+
+            var showFrom = document.getElementById("DisplayEditBooking");
+            var hideForm = document.getElementById("hideFrom");
+
+            showFrom.style.display = "block";
+            hideForm.style.display = "none";
+
+
+
+
+        })
+
+        .catch(function (error) {
+            displayError(error);
+        });
+
+}
+
+async function seditBooking() {
+
+    var editedBooking = JSON.stringify({
+
+
+
+        Id: document.getElementById("editbookingID").value,
+        PickupLocation: document.getElementById("editPickupLocation").value,
+        DropOffLocation: document.getElementById("editDropOffLocation").value,
+        CurrentPassenger: document.getElementById("editCurrentPassenger").value,
+        PassengerName: document.getElementById("editPassengerName").value,
+        VehicleId: document.getElementById("editVehicleId").value
+
+    });
+
+    console.log(document.getElementById("editbookingID"));
+
+
+    fetch("http://webteach_net.hallam.shu.ac.uk/cmsds/api/booking", {
+
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: editedBooking
+
+    })
+        .then(function (response) {
+            if (response.ok) {
+                fetchBookings();
+                var showFrom = document.getElementById("DisplayEditBooking");
+                var hideForm = document.getElementById("hideFrom");
+
+                showFrom.style.display = "none";
+                hideForm.style.display = "block";
+
+
+            }
+            else {
+
+                throw new Error('Oops. ' + response.status + ' ' + response.statusText);
+            }
+
+        })
+
+
 }
 
 
 function cancelAdd() {
-  var addBooking = document.getElementById("addBooking");
-  if (addBooking) {
-    addBooking.style.display = "none";
-  }
+    var addBooking = document.getElementById("addBooking");
+    if (addBooking) {
+        addBooking.style.display = "none";
+    }
 
-  var showBookings = document.getElementById("showBookings");
-  if (showBookings) {
-    showBookings.style.display = "block";
-  }
-}
+    var ShowForm = document.getElementById("BookingOut");
+    if (ShowForm) {
+        ShowForm.style.display = "block";
 
-function remove(booking) {
-  var index = Bookings.findIndex(function (item) {
-    return JSON.stringify(item) === JSON.stringify(booking);
-  });
-  if (index !== -1) {
-    Bookings.splice(index, 1);
-  }
-  var table = createTable(Bookings);
-  document.getElementById("CurrentTrips").innerHTML = table;
+    }
+
+
 }
 
 
-function remove(booking) {
-  var index = Bookings.findIndex(function (item) {
-    return item.id === booking.id;
-  });
-  if (index !== -1) {
-    Bookings.splice(index, 1);
-  }
-  var table = createTable(Bookings);
-  document.getElementById("CurrentTrips").innerHTML = table;
-}
+
+
 
 
 
